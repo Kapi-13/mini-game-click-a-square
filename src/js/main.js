879dd41da5813dc71b9d22d497ed7dfdb558e0be
Kapi-13 <rup.kacper.bo@gmail.com>
2026-02-19 16:45:33 +0100

    wprowadzeni js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 const startGameBtn = document.querySelector(".start-game-btn");
 const container = document.querySelector(".container");
+const square = document.querySelector(".square");
 startGameBtn.addEventListener("click", () => {
     container.classList.add("in-game");
 });
-setInterval(() => {}, 1000);
+setInterval(() => {
+    square.style.left = Math.floor(Math.random() * (600 - 0 + 1)) + 0 + "px";
+}, 1000);
+// container.classList.add("in-game");
