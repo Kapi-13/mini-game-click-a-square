de7231af58050e9a523803d4b6e2d60886e4d7f2
Kapi-13 <rup.kacper.bo@gmail.com>
2026-02-19 14:16:20 +0100

    Dodanie kwadratu do game-screen

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,8 @@
 const startGameBtn = document.querySelector(".start-game-btn");
 const container = document.querySelector(".container");
 startGameBtn.addEventListener("click", () => {
-    // container.classList.add("in-game");
+    container.classList.add("in-game");
 });
+setInterval(() => {}, 1000);
 
 //# sourceMappingURL=main.js.map
