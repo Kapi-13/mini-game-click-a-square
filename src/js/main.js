e7d4e5b733bfbc73955ddcda35445f252bb3d57c
Kapi-13 <rup.kacper.bo@gmail.com>
2026-02-20 10:06:06 +0100

    ostylowanie nowej struktury, js cz.1

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,21 @@
 const startGameBtn = document.querySelector(".start-game-btn");
 const container = document.querySelector(".container");
 const square = document.querySelector(".square");
+const points = document.querySelector(".points");
+
+let pointsNumber = 0;
+let leftDistance;
+
 startGameBtn.addEventListener("click", () => {
     container.classList.add("in-game");
+    square.addEventListener("click", () => {
+        pointsNumber++;
+        points.innerText = pointsNumber;
+    });
+    setInterval(() => {
+        leftDistance = Math.floor(Math.random() * (600 - 0 + 1)) + 0 + "px";
+        square.style.left = leftDistance;
+    }, 800);
 });
-setInterval(() => {
-    square.style.left = Math.floor(Math.random() * (600 - 0 + 1)) + 0 + "px";
-}, 1000);
+
 // container.classList.add("in-game");
