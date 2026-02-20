afbbd1e0d4093ba1d8cd2e206e98332204f7bd9e
Kapi-13 <rup.kacper.bo@gmail.com>
2026-02-20 16:25:45 +0100

    js cz.3, prowizoryczna gra

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,17 @@ const startGameBtn = document.querySelector(".start-game-btn");
 const container = document.querySelector(".container");
 const square = document.querySelector(".square");
 const points = document.querySelector(".points");
-const sound = new Audio(
-    "https://pixabay.com/sound-effects/technology-mouse-clicks-6849/",
-);
+const time = document.querySelector(".time");
+const endGameScreen = document.querySelector(".end-game-screen");
 
 let pointsNumber = 0;
-let leftDistance;
-let topDistance;
 let oneClick = 0;
+let timeNumber = 30;
+
+points.innerText = "0";
+time.innerText = "30";
 
 startGameBtn.addEventListener("click", () => {
-    sound.play();
     container.classList.add("in-game");
     square.addEventListener("click", () => {
         if (oneClick === 0) {
@@ -21,11 +21,19 @@ startGameBtn.addEventListener("click", () => {
             oneClick++;
         }
     });
-    setInterval(() => {
-        leftDistance = Math.floor(Math.random() * (600 - 0 + 1)) + 0 + "px";
+    const randomPositionInterval = setInterval(() => {
+        let leftDistance = Math.floor(Math.random() * (600 - 0 + 1)) + 0 + "px";
         square.style.left = leftDistance;
-        topDistance = Math.floor(Math.random() * (400 - 0 + 1)) + 0 + "px";
+        let topDistance = Math.floor(Math.random() * (400 - 0 + 1)) + 0 + "px";
         square.style.top = topDistance;
         oneClick = 0;
     }, 800);
+    const timeInterval = setInterval(() => {
+        timeNumber--;
+        time.innerText = timeNumber;
+        if (timeNumber === 0) {
+            clearInterval(timeInterval);
+            clearInterval(randomPositionInterval);
+        }
+    }, 1000);
 });
