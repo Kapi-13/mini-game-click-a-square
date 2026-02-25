7f9f3f744eea44806340400002f92cd215a4869e
Kapi-13 <rup.kacper.bo@gmail.com>
2026-02-25 20:06:05 +0100

    dodanie przyśpieszenia cz.1

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,32 @@
 // Pobranie elementów z DOM
-const startGameBtn = document.querySelector(".start-game-btn");
-const container = document.querySelector(".container");
-const square = document.querySelector(".square");
-const points = document.querySelector(".points");
-const time = document.querySelector(".time");
-const endGameScreen = document.querySelector(".end-game-screen");
-const totalScore = document.querySelector(".total-score");
-const restartGameBtn = document.querySelector(".restart-game-btn");
-const timeInputs = document.querySelectorAll(".time-mode-inputs input");
-const endInfinityGameBtn = document.querySelector(".end-game-btn");
+const startGameBtn = document.querySelector('.start-game-btn');
+const container = document.querySelector('.container');
+const square = document.querySelector('.square');
+const points = document.querySelector('.points');
+const time = document.querySelector('.time');
+const endGameScreen = document.querySelector('.end-game-screen');
+const totalScore = document.querySelector('.total-score');
+const restartGameBtn = document.querySelector('.restart-game-btn');
+const timeInputs = document.querySelectorAll('.time-mode-inputs input');
+const fasterModeInputs = document.querySelectorAll('.faster-mode-inputs input');
+const endInfinityGameBtn = document.querySelector('.end-game-btn');
 
 // Utworzenie obiektów audio (muzyka tła, dźwięk kliknięcia i koniec gry)
-const backgroundMusic = new Audio("./audio/HeadEmpty.mp3");
-const clickSound = new Audio("./audio/Click.wav");
-const endGameSound = new Audio("./audio/EndGame.wav");
+const backgroundMusic = new Audio('./audio/HeadEmpty.mp3');
+const clickSound = new Audio('./audio/Click.wav');
+const endGameSound = new Audio('./audio/EndGame.wav');
 
 // Zmienne sterujące stanem gry
 let pointsNumber = 0; // aktualna liczba punktów gracza
 let allTime; // czas gry w sekundach
+let faster; // przyśpieszenie gry
 let checkedElem; // zaznaczony input
 const GAME_WIDTH = 600;
 const GAME_HEIGHT = 400;
-const SQUARE_MOVE_INTERVAL = 800;
+let SQUARE_MOVE_INTERVAL = 800;
 let timeInterval;
 let randomPosition;
-endInfinityGameBtn.style.display = "none";
+endInfinityGameBtn.style.display = 'none';
 
 const stopAudio = (audioName) => {
     audioName.pause();
@@ -32,29 +34,28 @@ const stopAudio = (audioName) => {
 };
 
 const endGame = () => {
-    endInfinityGameBtn.style.display = "none";
-    container.classList.remove("in-game");
-    container.classList.add("end-game");
+    endInfinityGameBtn.style.display = 'none';
+    container.classList.remove('in-game');
+    container.classList.add('end-game');
     clearInterval(timeInterval);
     clearInterval(randomPosition);
     stopAudio(backgroundMusic);
     endGameSound.play();
-    allTime === "infinity"
-        ? (totalScore.innerText =
-              "Zdobyłeś " +
-              pointsNumber +
-              " punktów w " +
-              "nieskończonym czasie")
-        : (totalScore.innerText =
-              "Zdobyłeś " + pointsNumber + " punktów w " + allTime + " sekund");
+    allTime === 'infinity'
+        ? (totalScore.innerText = 'Zdobyłeś ' + pointsNumber + ' punktów w ' + 'nieskończonym czasie')
+        : (totalScore.innerText = 'Zdobyłeś ' + pointsNumber + ' punktów w ' + allTime + ' sekund');
     pointsNumber = 0;
 };
 
 // Obsługa kliknięcia przycisku startu gry
-startGameBtn.addEventListener("click", () => {
+startGameBtn.addEventListener('click', () => {
     timeInputs.forEach((element) => {
         element.checked ? (allTime = element.id) : [];
         console.log(allTime);
+    });
+    fasterModeInputs.forEach((element) => {
+        element.checked ? (faster = element.id) : [];
+        console.log(faster);
     });
     // Ustawienie początkowych wartości na ekranie
     points.innerText = pointsNumber;
@@ -66,29 +67,28 @@ startGameBtn.addEventListener("click", () => {
     backgroundMusic.play();
 
     // Przełączenie widoku na ekran gry
-    container.classList.add("in-game");
+    container.classList.add('in-game');
 
     // Interwał odpowiedzialny za losowe przemieszczanie kwadratu
-    randomPosition = setInterval(() => {
-        // Losowa pozycja pozioma (oś X)
-        let leftDistance =
-            Math.floor(Math.random() * (GAME_WIDTH - 0 + 1)) + 0 + "px";
-        square.style.left = leftDistance;
+    const setRandomPosition = () => {
+        randomPosition = setInterval(() => {
+            // Losowa pozycja pozioma (oś X)
+            let leftDistance = Math.floor(Math.random() * (GAME_WIDTH - 0 + 1)) + 0 + 'px';
+            square.style.left = leftDistance;
 
-        // Losowa pozycja pionowa (oś Y)
-        let topDistance =
-            Math.floor(Math.random() * (GAME_HEIGHT - 0 + 1)) + 0 + "px";
-        square.style.top = topDistance;
+            // Losowa pozycja pionowa (oś Y)
+            let topDistance = Math.floor(Math.random() * (GAME_HEIGHT - 0 + 1)) + 0 + 'px';
+            square.style.top = topDistance;
 
-        // Odblokowanie możliwości zdobycia punktu w nowej pozycji
-        canClickFlag = 0;
-    }, SQUARE_MOVE_INTERVAL);
-
+            // Odblokowanie możliwości zdobycia punktu w nowej pozycji
+            canClickFlag = 0;
+        }, SQUARE_MOVE_INTERVAL);
+    };
+    setRandomPosition();
     let timeNumber = allTime; // pozostały czas gry
-    console.log(timeNumber, allTime, time);
-    if (allTime === "infinity") {
-        time.innerText = "Nieskończony czas";
-        endInfinityGameBtn.style.display = "block";
+    if (allTime === 'infinity') {
+        time.innerText = 'Nieskończony czas';
+        endInfinityGameBtn.style.display = 'block';
     } else {
         // Interwał odliczający czas gry
         timeInterval = setInterval(() => {
@@ -102,10 +102,18 @@ startGameBtn.addEventListener("click", () => {
             }
         }, 1000);
     }
+
+    setTimeout(() => {
+        clearInterval(randomPosition);
+        setTimeout(() => {
+            SQUARE_MOVE_INTERVAL = 200;
+            setRandomPosition();
+        }, 2000);
+    }, 3000);
 });
 
 // Obsługa kliknięcia w kwadrat
-square.addEventListener("click", () => {
+square.addEventListener('click', () => {
     stopAudio(clickSound);
     // Odtworzenie efektu dźwiękowego
     clickSound.play();
@@ -115,10 +123,10 @@ square.addEventListener("click", () => {
     points.innerText = pointsNumber;
 });
 
-restartGameBtn.addEventListener("click", () => {
-    container.classList.remove("end-game");
+restartGameBtn.addEventListener('click', () => {
+    container.classList.remove('end-game');
 });
 
-endInfinityGameBtn.addEventListener("click", () => {
+endInfinityGameBtn.addEventListener('click', () => {
     endGame();
 });
